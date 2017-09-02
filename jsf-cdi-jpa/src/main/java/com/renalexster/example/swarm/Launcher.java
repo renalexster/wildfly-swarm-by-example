d039f9f807b76a406790828fc242a89582da92ca
renato <renalexster@gmail.com>
2017-09-01 21:43:56 -0300

    datasource

--- jsf-cdi-jpa/src/main/java/com/renalexster/example/swarm/Launcher.java
+++ jsf-cdi-jpa/src/main/java/com/renalexster/example/swarm/Launcher.java
@@ -6,14 +6,6 @@ import org.wildfly.swarm.container.DeploymentException;
 public class Launcher {
 	public static void main(String[] args) throws IllegalStateException, DeploymentException, Exception {
 		new Swarm()
-//		.fraction(new DatasourcesFraction()
-//                .dataSource("MyDS", (ds) -> {
-//                    ds.driverName("h2");
-//                    ds.connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-//                    ds.userName("sa");
-//                    ds.password("sa");
-//                })
-//)
 		.start().deploy();
 	}
 }
